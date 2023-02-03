c4175af2c530b57f8b8b42bd6a90688f5bb11ef3
bahaeddine <sihassi.bahaeddine@gmail.com>
2023-02-03 12:28:25 +0100

    setup

---
+++ resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<div class="rounded-full border-8 border-blue-700 shadow-xl">
+    <img src="{{URL('/imgs/profile.jpg')}}" class="h-36 rounded-full sm:h-56" alt="bahaeddine" width="230" height="160">
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,19 +12,14 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Styles -->
-        @livewireStyles
     </head>
-    <body class="font-sans antialiase bg-gray-100 dark:bg-gray-900d">
-        <x-jet-banner />
-
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @include('layouts.navigation')
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-white dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
@@ -36,9 +31,5 @@
                 {{ $slot }}
             </main>
         </div>
-
-        @stack('modals')
-
-        @livewireScripts
     </body>
 </html>
